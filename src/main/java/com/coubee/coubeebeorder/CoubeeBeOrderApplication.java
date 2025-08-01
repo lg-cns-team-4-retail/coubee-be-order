bcab5f00b95a3b1093c908bd099e4e6cb754e11e
Daehyun Kim <daizy13125@gmail.com>
2025-08-01 11:28:32 +0900

    Refactor: k8s 세팅 #2

--- src/main/java/com/coubee/coubeebeorder/CoubeeBeOrderApplication.java
+++ src/main/java/com/coubee/coubeebeorder/CoubeeBeOrderApplication.java
@@ -4,13 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
-@EnableDiscoveryClient // Kubernetes에서도 사용되는 동일한 어노테이션
 @EnableFeignClients
 @EnableJpaAuditing
 @EnableKafka
