2d39d8fcc558c478411cbf5f9549b886b0dc6e22
Daehyun Kim <daizy13125@gmail.com>
2025-08-06 10:49:08 +0900

    Fix: Import PortOnePaymentCancelRequest in PaymentServiceImpl

--- src/main/java/com/coubee/coubeebeorder/service/PaymentServiceImpl.java
+++ src/main/java/com/coubee/coubeebeorder/service/PaymentServiceImpl.java
@@ -16,7 +16,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import com.coubee.coubeebeorder.remote.dto.PortOnePaymentCancelRequest;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import java.util.stream.Collectors;
