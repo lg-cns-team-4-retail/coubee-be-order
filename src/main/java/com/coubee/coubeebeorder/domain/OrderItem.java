70f2870950f4154eecf4aac0138d3dd90de5cd82
Daehyun Kim <daizy13125@gmail.com>
2025-09-01 12:55:34 +0900

    fix: version up to delete cache

--- src/main/java/com/coubee/coubeebeorder/domain/OrderItem.java
+++ src/main/java/com/coubee/coubeebeorder/domain/OrderItem.java
@@ -25,7 +25,7 @@ public class OrderItem extends BaseTimeEntity {
     @Column(name = "product_id", nullable = false)
     private Long productId;
 
-    @Column(name = "product_name", nullable = false)
+    @Column(name = "product_name", nullable = false, columnDefinition = "VARCHAR(255)")
     private String productName;
 
     @Column(nullable = false)
